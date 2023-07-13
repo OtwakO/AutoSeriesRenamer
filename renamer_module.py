c05c75d5ae450e2f77133f48fefecd63e8f47915
OtwakO <78617209+tom28055101@users.noreply.github.com>
2023-07-13 11:51:46 +0800

    Update renamer_module.py
    Add cookies

--- renamer_module.py
+++ renamer_module.py
@@ -2,12 +2,23 @@
 import os, sys, time
 import asyncio, json
 from loguru import logger
+from pathlib import Path
 from decouple import config, UndefinedValueError
 from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
 
 os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
 
 PROMPT = '請你扮演一個檔案命名工具，我會以List的資料格式給予你許多的檔案名稱，你要將這些檔案名稱重新組合為 "節目名稱 S01E01" 這類的名稱，如果檔案名稱內有包含中文劇名請優先使用，如果沒有中文劇名的話請務必先線上查找中文劇名，如果檔案名稱看起來是劇場版或是特別篇就在劇名後面加上劇場版或是特別篇並以第0季來命名並，第幾季通常會以S1, S2之類的格式來辨別。 回答我的時候請只給予我重新組合的名稱不要有任何其他的字語。回答不要包含類似"好的，新的檔案名稱是"之類的詞語。單獨的把新的檔案名稱以List的資料格式給我。'
+
+Path(f"bing_cookies").mkdir(parents=True, exist_ok=True)
+try:
+    cookies = json.loads(
+        open("bing_cookies/bing_cookies_1.json", encoding="utf-8").read()
+    )
+except Exception as e:
+    cookies = None
+    logger.error(e)
+    logger.info("No cookies loaded")
 
 
 async def renamer_engine(series_list, proxy):
@@ -16,12 +27,12 @@ async def renamer_engine(series_list, proxy):
         try:
             if proxy:
                 logger.info(f"Proxy configured.")
-                bot = await Chatbot.create(proxy=proxy)
+                bot = await Chatbot.create(proxy=proxy, cookies=cookies)
             elif not proxy:
                 logger.info(f"No proxy configured.")
                 bot = await Chatbot.create()
         except Exception as e:
-            logger.trace(e)
+            logger.error(e)
             logger.error("Error occurred, try again later.")
             os._exit(0)
 
@@ -42,7 +53,7 @@ async def renamer_engine(series_list, proxy):
             await bot.close()
             break
         except Exception as e:
-            logger.trace(e)
+            logger.error(e)
             await bot.close()
             continue
     logger.info(f"Fetching finished.")
